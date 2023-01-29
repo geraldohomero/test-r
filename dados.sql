f7ae5cb27ec992ab9c73660d14e63bca79714e7b
Geraldo Homero <geraldohomero@protonmail.com>
2023-01-28 23:29:31 -0300

    Update dados.sql

--- dados.sql
+++ dados.sql
@@ -1,9 +1,7 @@
--- Create the table
 CREATE TABLE example_data (
   x INTEGER,
   y INTEGER
 );
 
--- Insert data into the table
 INSERT INTO example_data (x, y)
 VALUES (1, 2), (2, 4), (3, 6), (4, 8), (5, 10);
